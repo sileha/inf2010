5be2a193c204f1464f59f9a5cff658454ff06f8d
sileha <sid.ahmed_lehah@yahoo.fr>
2017-10-10 16:07:03 -0400

    version fonctionnelle du premierr partie

--- TP3/src/QuadraticSpacePerfectHashing.java
+++ TP3/src/QuadraticSpacePerfectHashing.java
@@ -1,116 +1,143 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class QuadraticSpacePerfectHashing<AnyType>
+public class QuadraticSpacePerfectHashing<AnyType> 
 {
-   static int p = 46337;
+	static int p = 46337;
 
-   int a, b, m;
-   AnyType[] items;
+	int a, b, m;
+	AnyType[] items;
 
-   QuadraticSpacePerfectHashing()
-   {
-      a=b=0; items = null;
-   }
+	QuadraticSpacePerfectHashing()
+	{
+		a=b=0; items = null;
+	}
 
-   QuadraticSpacePerfectHashing(ArrayList<AnyType> array)
-   {
-      AllocateMemory(array);
-   }
+	QuadraticSpacePerfectHashing(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
 
-   public void SetArray(ArrayList<AnyType> array)
-   {
-      AllocateMemory(array);
-   }
+	public void SetArray(ArrayList<AnyType> array)
+	{
+		AllocateMemory(array);
+	}
 
-   public int Size()
-   {
-      if( items == null ) return 0;
+	public int Size()
+	{
+		if( items == null ) return 0;
 
-      return items.length;
-   }
+		return items.length;
+	}
 
-   public boolean containsKey(int key)
-   {
-      // A completer
-      if(items[key] != null)
-         return false;
-      return true;
-   }
+	public boolean containsKey(int key)
+	{
+		// A completer            voir est ce il ya un element a ce key 
+		if (items[key] == null )
+		{
+			return false;
+		}
+		return true;
+	}
 
-   public boolean containsValue(AnyType x ) {
+	public boolean containsValue(AnyType x )
+	{
+		// A completer
+		int key =  getKey(x);
+		if (items[key] != null && items[key].equals(x))
+		{
+			return true;
+		}
+		return false;
+		// on doit verifier que la valeur est la meme celle de x et la valeur dans le tableau en key x
 
-      /** Allowed to change method? */
-      if (Size() == 0) return false;
+	}
 
-      int index = getKey(x);
-      if (items[index] != null)
-         return true;
+	public void remove (AnyType x) {
+		// A completer
+		items[getKey(x)] = null;
 
-      return false;
-   }
+	}
 
-   public void remove (AnyType x) {
-      // A completer
-      int temp = getKey(x);
-      items[temp] = null;
-   }
+	public int getKey (AnyType x) {
+		
+		
+		return ( ( a*(x).hashCode() + b ) % p ) % m;
+	}
 
-   public int getKey (AnyType x) {
-      // A completer
-      return ( ( a*x.hashCode() + b ) % p ) % m;
-   }
+	@SuppressWarnings("unchecked")
+	private void AllocateMemory(ArrayList<AnyType> array)
+	{
+		Random generator = new Random( System.nanoTime() );
+		m = (int) Math.pow(array.size(), 2);
+		items = (AnyType[]) new Object[m];
+		
+		
+		if(array == null || array.size() == 0)
+		{
+			
+			return;
+		}
+		if(array.size() == 1)
+		{
+			a = b = 0;
 
-   @SuppressWarnings("unchecked")
-   private void AllocateMemory(ArrayList<AnyType> array)
-   {
-      Random generator = new Random( System.nanoTime() );
+			items[getKey(array.get(0))]	= array.get(0);
+			return;
+		}
+		
+		if (array.size() > 1)
+		{
+			a = generator.nextInt(p);
+			b = generator.nextInt(p);
+			boolean tableauRemplieSansCollision = true;
+			
+			while (tableauRemplieSansCollision)
+				{
+				
+					for (int i=0; i<array.size() ;i++)
+						{
+						int key = getKey(array.get(i));
+							if (!containsKey(key))
+								{
+									items[key] = array.get(i);
+									if (i == array.size()-1)
+									{
+										tableauRemplieSansCollision = false;
+									}
+					
+								}
+							else if (!containsValue(array.get(i)))
+								{
+									tableauRemplieSansCollision = false;
+									makeEmpty();
+									break;
+								}
+							
+						}
+				}
+		}
+	}
 
+	
+	
+	public String toString () {
+		String result = "";
+		for (int i=0; i<items.length; i++)
+		{
+			if (!items[i].equals(null))
+			{
+				result.concat( ", (" + getKey(items[i]) + ","+ items[i] + ")");
+			}
+			
+		}
+		return result; 
+	}
 
-      m = (int) (Math.pow(array.size(), 2));
-      if(array == null || array.size() == 0)
-      {
-         // A completer
-         return;
-      }
-      if(array.size() == 1)
-      {
-         a = b = 0;
-         items = (AnyType[]) new Object[(int) (Math.pow(array.size(), 2))];
-         int index = ( ( a*array.get(0).hashCode() + b ) % p ) % m;
-         items[index] = array.get(0);
-         return;
-      }
-      // A completer
-      if(array.size() > 1) {
-         items = (AnyType[]) new Object[(int) (Math.pow(array.size(), 2))];
-         a = generator.nextInt(p);
-         b = generator.nextInt(p);
-         for(AnyType item: array) {
-            int index = ( ( a*item.hashCode() + b ) % p ) % m;
-            items[index] = item;
-         }
-      }
-
-   }
-
-
-
-   public String toString () {
-      String result = "";
-
-      // A completer
-      for(int i = 0; i < items.length - 1; i++) {
-          if (items[i] != null)
-              result = result.concat("( " + getKey(items[i]) + " , " + items[i] + " )");
-          // A completer
-      }
-      return result;
-   }
-
-   public void makeEmpty () {
-      // A completer
-      if (this.Size() != 0)
-         items = null;
-   }
+	public void makeEmpty () {
+		   for (int i = 0; i<items.length; i++)
+		   {
+			   items[i] = null ;
+		   }
+   	}
 }
