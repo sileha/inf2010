7a1f41e95049eacf847f0b741e0e06947ac97ba7
sileha <sid.ahmed_lehah@yahoo.fr>
2017-10-31 21:14:12 -0400

    ma partie bien complete et fonctionelle

--- TP4/AvlTree.java
+++ TP4/AvlTree.java
@@ -16,32 +16,73 @@ public class AvlTree<T extends Comparable<T>> extends BST<T>
         int heightDiff = Math.abs(getHeight(node.left) - getHeight(node.right));
 
         return childsBalanced && heightDiff <= 1;
-    }
+    }	
 
     public void insert(T elem) { root = insert(root, elem); }
 
     private Node<T> insert(Node<T> node, T elem)
     {
-        // À compléter
+       if (node ==  null)
+       {
+    	   return new Node(elem);
+       }
+       int comparaison = elem.compareTo(node.val);
+       if (comparaison < 0)
+       {
+    	   node.left = insert(node.left, elem);
+    	   if (getHeight(node.left) - getHeight(node.right) == 2)
+    	   {
+    		   if (elem.compareTo(node.left.val) < 0)
+    		   {
+    			   node = balanceLeftLeft(node);
+    		   }
+    		   else 
+    			   node = balanceLeftRight(node);
+    	   }
+       }
+       else if (comparaison > 0)
+       {
+    	   node.right = insert(node.right, elem);
+    	   if (getHeight(node.right) - getHeight(node.left) == 2)
+    	   {
+    		   if (elem.compareTo(node.right.val) > 0)
+    		   {
+    			   node = balanceRightRight(node);
+    		   }
+    		   else 
+    			   node = balanceRightLeft(node) ; 
+    	   }
+       }
+       else ;
+       
+       return node;
     }
 
     private Node<T> balanceRightRight(Node<T> node)
     {
-        // À compléter
+       Node<T> temp = node.right;
+       node.right = temp.left ;
+       temp.left = node;
+       return temp;
     }
 
     private Node<T> balanceRightLeft(Node<T> node)
     {
-        // À compléter
+        node.right = balanceLeftLeft(node.right);
+        return balanceRightRight(node);
     }
 
     private Node<T> balanceLeftLeft(Node<T> node)
     {
-        // À compléter
+        Node<T> temp = node.left;
+        node.left = temp.right;
+        temp.right = node;
+        return temp;
     }
 
     private Node<T> balanceLeftRight(Node<T> node)
     {
-        // À compléter
+        node.left = balanceRightRight(node.left);
+        return balanceLeftLeft(node);
     }
 }
