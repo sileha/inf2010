cec2edc8081dfb0bea351815d7369062be796463
Jonathan <jonathanoneill97@hotmail.com>
2017-11-19 01:53:04 -0500

    modifications de fusion ... inserted agit bizarrement ...

--- TP5/Monceau.java
+++ TP5/Monceau.java
@@ -12,24 +12,17 @@ public class Monceau {
     public void fusion(Monceau autre) {
         // à compléter
         ArrayList<Node> retenue = new ArrayList<Node>();
-        int ordreMax = (this.ordreMax() > autre.ordreMax() ? this.ordreMax(): autre.ordreMax());
+        int oMax = (this.ordreMax() > autre.ordreMax() ? this.ordreMax(): autre.ordreMax());
+        ArrayList<Node> tmp = new ArrayList<Node>();
 
-        for(int j = 0; j < ordreMax; j++) {
-            Node node1[] = arbreMemeOrdre(this.arbres, j);
-            Node node2[] = arbreMemeOrdre(autre.arbres, j);
-
-            if(retenue.size() != 0) {
-                Node node3[] = new Node[retenue.size()];
-                node3 = arbreMemeOrdre(retenue, j);
-                remplirTableau(arbres, retenue, node3);
+        for(int j = 0; j <= oMax; j++) {
+                arbreMemeOrdre(this.arbres, tmp, j);
+                arbreMemeOrdre(autre.arbres, tmp, j);
+                arbreMemeOrdre(retenue, tmp, j);
+                remplirTableau(this.arbres, retenue, tmp);
             }
-
-            if(node1.length != 0)
-                remplirTableau(arbres, retenue, node1);
-            if(node2.length != 0)
-                remplirTableau(arbres, retenue, node2);
         }
-    }
+
     public void insert(int val) {
         // à compléter
         Node newNode = new Node(val);
@@ -42,48 +35,60 @@ public class Monceau {
         // à compléter
         for(Node i: this.arbres) {
             Node foundNode = i.findValue(val);
-            if(foundNode != null)
+            if(foundNode != null) {
+                i.delete();
                 return true;
+            }
         }
         return false;
     }
 
     public void print() {
         // à compléter
-        for(Node i: this.arbres)
+        int count = 1;
+        for(Node i: this.arbres) {
+            System.out.println("Arbres: " + count++);
             i.print(" ");
-    }
-
-    public Node[] arbreMemeOrdre(ArrayList<Node> a, int ordre) {
-        Node array[] = new Node[a.size()];
-        int counter = 0;
-        for(Node i: a) {
-            if(i.ordre == ordre)
-                array[counter++] = i;
         }
-        return array;
     }
 
-    public void remplirTableau(ArrayList<Node> arbres, ArrayList<Node> arbresTmp, Node node[]) {
-        if (node.length == 1) {
-            arbres.add(node[0]);
-        } else if (node.length == 2) {
+    public void arbreMemeOrdre(ArrayList<Node> a, ArrayList<Node> tmp, int ordre) {
+        if(a.size() != 0) {
+            for (Node i : a) {
+                if (i.ordre == ordre)
+                    tmp.add(i);
+            }
+        }
+    }
+
+    public void remplirTableau(ArrayList<Node> arbresFinal, ArrayList<Node> retenue, ArrayList<Node> tmp) {
+        if(tmp.size() == 1) {
+            Node node1 = tmp.get(0);
+            arbresFinal.add(node1);
+        }
+        if(tmp.size() == 2) {
+            Node node1 = tmp.get(0);
+            Node node2 = tmp.get(1);
             try {
-                node[0].fusion(node[1]);
+                node1.fusion(node2);
             } catch (DifferentOrderTrees e) {};
-            arbresTmp.add(node[0]);
-        } else if(node.length == 3) {
+            retenue.add(node1);
+        }
+        if(tmp.size() == 3) {
+            Node node1 = tmp.get(0);
+            Node node2 = tmp.get(1);
             try {
-                node[0].fusion(node[1]);
+                node1.fusion(node2);
             } catch (DifferentOrderTrees e) {};
-            arbres.add(node[0]);
-            arbresTmp.add(node[2]);
+            retenue.add(node1);
+            arbresFinal.add(tmp.get(2));
         }
     }
 
     public int ordreMax() {
         int max = 0;
         for(Node i: this.arbres) {
+            //System.out.println(i.ordre);
             if(i.ordre > max)
                 max = i.getVal();
         }
