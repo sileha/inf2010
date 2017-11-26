0a9ce8cbd39d09fc22fdca9c155e789e6acc2d2c
sileha <sid.ahmed_lehah@yahoo.fr>
2017-11-26 16:05:40 -0500

    exercice 1 complet et fonctionnel

--- TP6/CircularDeps/CodeBase.java
+++ TP6/CircularDeps/CodeBase.java
@@ -1,6 +1,7 @@
 package CircularDeps;
 
 import java.util.HashSet;
+import java.util.*;
 
 public class CodeBase {
     private SourceFile[] sourceFiles;
@@ -21,11 +22,76 @@ public class CodeBase {
         return false;
     }
 
+    Stack<SourceFile> stack = new Stack();
+	ArrayList<SourceFile> visited = new ArrayList();
+    
     // DFS partant de <<file>> détectant s'il existe
     // des dépendances circulaires dans les fichiers source.
     private boolean existsCircularDependencies(SourceFile file, HashSet<SourceFile> dependentFiles)
     {
-        // À compléter
-        return false;
+    	
+	    	if (isCyclic(file, dependentFiles))
+	    	{
+	    		return true;
+	    	}
+
+    	return false;
+    }
+    public boolean isCyclic(SourceFile file, HashSet<SourceFile> dependentFiles)
+    {
+    	if (!isVisited(visited,file))
+    	{
+    		stack.add(file);
+        	visited.add(file);
+        	for (SourceFile temp :file.getDependencies())
+        	{
+        		if (!isVisited(visited,temp) && isCyclic(temp,dependentFiles))
+        		{
+        			dependentFiles.add(file);
+        			if(!verifierPresence(dependentFiles, temp))
+        				dependentFiles.add(temp);
+        				
+        			return true;
+        		}
+        		else if (isInRecursion(stack,temp))
+        		{
+        			return true;
+        		}
+        	}
+    	}
+    	stack.remove(file);
+    	return false;
+    }
+    
+    public boolean verifierPresence(HashSet<SourceFile> dependentFiles,SourceFile file)
+    {
+    	for (SourceFile temp : dependentFiles)
+    	{
+    		if (temp.equals(file))
+    			return true;	
+    	}
+    	return false;
+    }
+    
+    public boolean isInRecursion(Stack<SourceFile> stack, SourceFile file)
+    {
+    	for(int i=0; i < stack.size() ; i++ )
+    	{
+    		if (stack.get(i).equals(file))
+    			return true;
+    	}
+    	return false;
+    }
+    
+    public boolean isVisited(ArrayList<SourceFile> liste, SourceFile file )
+    {
+    	for (int i = 0; i < liste.size() ; i++)
+    	{
+    		if (liste.get(i).equals(file))
+    		{
+    			return true;
+    		}
+    	}
+    	return false;
     }
 }
